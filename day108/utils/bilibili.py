661efa130a5378b4a1e7bf3e7d7035fc1d98b85c
wx <813703110@qq.com>
2022-08-15 01:03:51 +0800

    day108学习ajax

--- day108/utils/bilibili.py
+++ day108/utils/bilibili.py
@@ -17,9 +17,10 @@ def parse_page(url, timestamp):
         localtime = time.localtime(created_timestamp)
         created = time.strftime("%Y-%m-%d %H:%M:%S", localtime)
         if created_timestamp > timestamp:
-            # print(author, title, href, created)
-            data_list.append([author, title, href, created])
-        # data_list.append([author, title, href, created])
+            ...
+        #     # print(author, title, href, created)
+        #     data_list.append([author, title, href, created])
+        data_list.append([author, title, href, created])
 
     return data_list
 
@@ -29,7 +30,7 @@ def mid_data(mid):
     api_url = f'https://api.bilibili.com/x/space/arc/search?mid={mid}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp'
 
     # 1小时的时间戳3600
-    timestamp = time.time() - 3600 * 24 * 2
+    timestamp = time.time() - 3600 * 24 * 7
 
     # 解析一页，返回data_list
     data_mid = parse_page(api_url, timestamp)
